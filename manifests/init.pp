f0a29c72bc60c5e13c77a13394b6d5ab19886e78
Joe Julian <me@joejulian.name>
2013-04-23 17:04:51 -0700

    allow greater configuration flexibility

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,46 +1,38 @@
-class beaver {
-  package { "beaver":
-    provider => "pip",
-    ensure   => installed,
-    source   => "git+git://github.com/joejulian/beaver.git",
-    require  => [ 
-      Package['python-pip'],
-      Package['git'],
-      Package['ujson'],
-      Package['python-argparse'],
-      Package['python-pika'],
-      Package['python-redis'],
-      Package['python-ordereddict'],
-      Package['python-daemon'],
-    ],
-  }
+class beaver (
+  $init_path       = "/etc/init.d/beaver",
+  $service_name    = "beaver",
+  $ini_path        = "/etc/beaver.conf",
+  $redis_url       = "redis://logstash:6379/0",
+  $redis_namespace = "logstash"
+) {
+  include packages::beaver
 
-  file { "/etc/init.d/beaver":
+  file { $init_path:
     mode   => 755,
     source => "puppet:///modules/beaver/init.d/beaver",
   }
 
-  service { "beaver":
-    require   => File[ "/etc/init.d/beaver" ],
+  service { $service_name:
+    require   => File[ $init_path ],
     ensure    => running,
     enable    => true,
   }
 
   ini_setting { "beaver redis_url":
-    path    => '/etc/beaver.conf',
+    path    => $ini_path,
     section => 'beaver',
     setting => 'redis_url',
-    value   => 'redis://logstash:6379/0',
+    value   => $redis_url,
     ensure  => present,
-    notify  => Service['beaver'],
+    notify  => Service[$service_name],
   }
 
   ini_setting { "beaver redis_namespace":
-    path    => '/etc/beaver.conf',
+    path    => $ini_path,
     section => 'beaver',
     setting => 'redis_namespace',
-    value   => 'logstash',
+    value   => $redis_namespace,
     ensure  => present,
-    notify  => Service['beaver'],
+    notify  => Service[$service_name],
   }
 }
